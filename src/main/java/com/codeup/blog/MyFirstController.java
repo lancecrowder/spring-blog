887081c595f845eba090d71279a43c3e2d668a9f
lancecrowder <thelancecrowder@gmail.com>
2018-06-18 10:57:54 -0500

    Home Controller Complete

--- src/main/java/com/codeup/blog/MyFirstController.java
+++ src/main/java/com/codeup/blog/MyFirstController.java
@@ -2,6 +2,7 @@ package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,4 +12,33 @@ public class MyFirstController {
     public @ResponseBody String myFirstSpringView(){
         return "Hello from Spring!";
     }
+
+    @GetMapping("say-goodbye")
+    public @ResponseBody String mySecondSpringView(){
+        return "Goodbye from Spring!";
+    }
+
+    @GetMapping("/say-hello/{name}")
+    public @ResponseBody String sayHello(@PathVariable String name){
+        return String.format("Hello, %s!", name);
+    }
+
+    @GetMapping("/flights/from/{from}/to/{to}")
+    public @ResponseBody String getFlights(
+            @PathVariable String from,
+            @PathVariable String to
+    ){
+        return String.format("Finding flights from %s to %s....", from, to);
+    }
+
+    @GetMapping("/increment/{n}")
+    public @ResponseBody String increment(@PathVariable int n){
+        return String.format("%d + 1 = %d", n, n+1);
+    }
+
+    @GetMapping("/HomeController")
+    public @ResponseBody String HomeController(){
+        return "This is the landing page!";
+    }
+
 }
