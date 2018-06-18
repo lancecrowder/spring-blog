ed70b6f6c5767abea15146fcf22f287f5344b289
lancecrowder <thelancecrowder@gmail.com>
2018-06-18 10:38:40 -0500

    return "Hello from Spring!";

---
+++ src/main/java/com/codeup/blog/MyFirstController.java
@@ -0,0 +1,14 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstController {
+
+    @GetMapping("/say-hello")
+    public @ResponseBody String myFirstSpringView(){
+        return "Hello from Spring!";
+    }
+}
