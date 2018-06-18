6c182393801642ce9b0b2ca5294b2767d556e0b0
lancecrowder <thelancecrowder@gmail.com>
2018-06-18 16:57:14 -0500

    Passing data to views complete!

---
+++ src/main/java/com/codeup/blog/DiceController.java
@@ -0,0 +1,25 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String dice(){
+        return "/roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String play(@PathVariable int n, Model model) {
+        int number = (int )(Math.random() * 6 + 1);
+        boolean result = (n == number);
+        model.addAttribute("result", result);
+        model.addAttribute("number", number);
+        return "dice-rolled";
+    }
+
+}
