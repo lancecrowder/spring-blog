00701d3a784f7efe2c8b6b3f6729ac6e226a198b
lancecrowder <thelancecrowder@gmail.com>
2018-06-22 11:52:56 -0500

    Prettying up the NavBar

--- src/main/java/com/codeup/blog/PostService.java
+++ src/main/java/com/codeup/blog/PostService.java
@@ -1,48 +1,37 @@
 package com.codeup.blog;
 
-import com.codeup.blog.Repositories.PostRepository;
 import com.codeup.blog.models.Post;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codeup.blog.Repositories.PostRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class PostService {
+    private final PostRepository postRepository;
 
-    private PostRepository postRepository;
-    @Autowired
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
 
-    private List<Post> posts;
-
-//    public PostService() {
-//        posts = new ArrayList<>();
-//        createPosts();
-//    }
-
     public List<Post> findAll() {
-        return posts;
+        return (List<Post>) postRepository.findAll();
     }
 
-//    public static Post save(Post post) {
-//        post.setId((posts.size() + 1));
-//        posts.add(post);
-//        return post;
-//    }
+    public Post save(Post post) {
+        postRepository.save(post);
+        return post;
+    }
 
     public Post findOne(long id) {
-        return posts.get((int) (id - 1));
+        return  postRepository.findOne(id);
     }
 
-//    private void createPosts() {
-//        Post post1 = new Post( 1, "Post 1", "Some stuff about post 1");
-//        Post post2 = new Post(2, "Post 2", "Some stuff about post 2");
-//        Post post3 = new Post(3, "Post 3", "Some stuff about post 3");
-//        save(post1);
-//        save(post2);
-//        save(post3);
-//    }
+    public Post editPost(Post post){
+        return postRepository.save(post);
+    }
+
+    public void deletePost(Long id){
+        postRepository.delete(id);
+    }
 }
--- src/main/java/com/codeup/blog/Repositories/PostRepository.java
+++ src/main/java/com/codeup/blog/Repositories/PostRepository.java
@@ -3,5 +3,8 @@ package com.codeup.blog.Repositories;
 import com.codeup.blog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository <Post, Long> {
+//    List<Post> findByTitleLike();
 }
