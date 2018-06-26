5eb26886f5606b4cf2440682d7fe06b1ae220fb6
lancecrowder <thelancecrowder@gmail.com>
2018-06-26 09:24:19 -0500

    Sec done with an error in UserDetailsLoader - fixed

--- src/main/java/com/codeup/blog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/blog/services/UserDetailsLoader.java
@@ -1,5 +1,6 @@
 package com.codeup.blog.services;
 
+import com.codeup.blog.Repositories.UserRepository;
 import com.codeup.blog.models.User;
 import com.codeup.blog.models.UserWithRoles;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,15 +10,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsLoader implements UserDetailsService {
-    private final User user;
+    private final UserRepository users;
 
-    public UserDetailsLoader(User user) {
-        this.user = user;
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = user.findByUsername(username);
+        User user = users.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("No user found for " + username);
         }
